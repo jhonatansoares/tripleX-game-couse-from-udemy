2b2436934a529ef5df6b4e9c7111d3dc082e0965
jhonatan <jhonatansoares62@gmail.com>
2020-05-17 14:20:22 -0300

    tx1 firt version of the tripleX game

--- tripleX.cpp
+++ tripleX.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
 #include <ctime>
+using namespace std; 
 
 int main(){
+    cout << "You are a super secret agent breaking into a server room";
+    cout << endl;
+    cout << "Enter de correct code to continue...";
+    cout << endl;
+
+    int codeA = 1;
+    int codeB = 2;
+    int codeC = 3;
+
+    int sum = codeA + codeB + codeC;
+    int product = codeA * codeB * codeC;
+
+    cout << endl;
+    cout << "There are tre numbers in the code." << endl;
+    cout << "The code add-up to :" << sum << endl;
+    cout << "The codes multiply to give: " << product << endl;
+
+    int guessA , guessB , guessC;
+    cin >> guessA;
+    cin >> guessB;
+    cin >> guessC;
+
+    cout << "You entered : " << guessA << guessB << guessC << endl;
+
+    int guessSum = guessA + guessB + guessC;
+    int guessProduct = guessA * guessB * guessC;
+
+    if(guessSum == sum && guessProduct == product){
+        cout << "You win!" << endl;
+    }else{
+        cout << "You loose!" << endl;
+    }
 
     return 0;
 }
